e38c419d06e7d1ed66041485c1d8132ac819d3f8
Adam Friedl <ajfriedl@gmail.com>
2017-08-07 11:23:19 -0400

    Successfully fetch API and return promise

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import { Banner, Heading } from 'rebass'
 import rikersIsland from './assets/images/rikersIsland.jpg'
+import fetch from 'isomorphic-fetch'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      totalCount: 0
+    }  
+  }
+
+  componentDidMount() { 
+    const count = fetch('https://data.cityofnewyork.us/resource/gqrb-77i6.json?$limit=12000')
+      .then(res => res.json())
+      .then(obj => 
+        this.setState({
+          totalCount: obj.length
+        })
+      )
+  }
+  
   render() {
     return (
       <div>
@@ -13,8 +31,10 @@ class App extends Component {
           backgroundImage='https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20'>
           <Heading
             f={[ 4, 5, 6, 7 ]}>
-            Rikers Numbers Today
+            Rikers Numbers Today<br />
+            {this.state.totalCount}
           </Heading>
+
         </Banner>
 
 
