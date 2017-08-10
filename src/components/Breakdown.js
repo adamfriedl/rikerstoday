dbe6bf205850ee8c88b27e1589b4c96de5820700
Adam Friedl <ajfriedl@gmail.com>
2017-08-10 13:55:07 -0400

    Various format changes

--- src/components/Breakdown.js
+++ src/components/Breakdown.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, PanelHeader, Text, Box, Panel, Subhead} from 'rebass'
+import { Flex, PanelHeader, Text, Box, Panel, PanelFooter, Subhead} from 'rebass'
 import { VictoryChart, VictoryContainer, VictoryPie, VictoryTheme, VictoryTransition, VictoryLabel, VictoryTooltip } from 'victory'
 import PieChart from './PieChart'
 import TextStat from './TextStat'
@@ -11,8 +11,14 @@ const Breakdown = props => {
   //   let admitDate = .admitted_dt
   //   console.log(today, admitDate)
   // }
+  
+  
+  // Below are the calculations on the fetched data set
+  
+  // Total population calculation
   let total = props.data.length
 
+  // Custody Level calculations
   let min = props.data.filter(inmate => inmate.custody_level === 'MIN').length
   let med = props.data.filter(inmate => inmate.custody_level === 'MED').length
   let max = props.data.filter(inmate => inmate.custody_level === 'MAX').length
@@ -22,6 +28,7 @@ const Breakdown = props => {
           { x: 'Max \n' + Math.round(max/total*100) + '%', y: max }
         ]
 
+  // Race calculations
   let black = props.data.filter(inmate => inmate.race === 'B').length
   let white = props.data.filter(inmate => inmate.race === 'W').length
   let asian = props.data.filter(inmate => inmate.race === 'A').length
@@ -34,19 +41,27 @@ const Breakdown = props => {
         { x: 'White \n' + Math.round(white/total*100) + '%', y: white }
       ]
 
-  let under18 = props.data.filter(inmate => inmate.age < 18).length
-  let to30 = props.data.filter(inmate => inmate.age > 18 && inmate.age <= 30).length
-  let to55 = props.data.filter(inmate => inmate.age > 30 && inmate.age <= 55).length
-  let over55 = props.data.filter(inmate => inmate.age > 55).length
+  // Age calculations
+  let to21 = props.data.filter(inmate => inmate.age <= 21).length
+  let to40 = props.data.filter(inmate => inmate.age > 21 && inmate.age <= 40).length
+  let over40 = props.data.filter(inmate => inmate.age > 40).length
   let ageChartData = [
-    { x: 'Under 18 \n' + Math.round(under18/total*100) + '%', y: under18 },
-    { x: '18 to 30 \n' + Math.round(to30/total*100) + '%', y: to30 },
-    { x: '31 to 55 \n' + Math.round(to55/total*100) + '%', y: to55 },
-    { x: 'Over 55 \n' + Math.round(over55/total*100) + '%', y: over55 },
+    { x: 'Under 22 \n' + Math.round(to21/total*100) + '%', y: to21 },
+    { x: '22 to 40 \n' + Math.round(to40/total*100) + '%', y: to40 },
+    { x: 'Over 40 \n' + Math.round(over40/total*100) + '%', y: over40 }
   ]
 
+  // Gender calculations
   let female = props.data.filter(inmate => inmate.gender === 'F').length
   let femalePercent = Math.round(female/total*100) + '%'
+
+  // Under mental health observation calculations
+  let mentalHealth = props.data.filter(inmate => inmate.bradh === 'Y').length
+  let mentalHealthPercent = Math.round(mentalHealth/total*100) + '%'
+
+  // Gang affiliation calculations
+  let gangAffiliation = props.data.filter(inmate => inmate.srg_flg === 'Y').length
+  let gangAffiliationPercent = Math.round(gangAffiliation/total*100) + '%'
 
 
 
@@ -73,6 +88,16 @@ const Breakdown = props => {
         <TextStat
           title={'Female'}
           stat={femalePercent}
+        />
+
+        <TextStat
+          title={'Mental Health'}
+          stat={mentalHealthPercent}
+        />
+
+        <TextStat
+          title={'Gang Affiliation'}
+          stat={gangAffiliationPercent}
         />
           
       </Flex>
