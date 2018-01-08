80f9009fd2593c7a77abde627673f3c7bb4f67d5
Adam Friedl <adamfriedl@Adams-MacBook-Pro.local>
2018-01-08 15:54:52 -0500

    moved bg image to imgur

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,45 +1,31 @@
-import React, { Component } from 'react'
-import { Flex, Box, Banner, Text, Heading } from 'rebass'
-import rikersCell from '../assets/images/rikersCell.jpg'
-import grid from '../assets/icons/grid.svg'
+import React, { Component } from 'react';
+import { Flex, Box, Banner, Text, Heading } from 'rebass';
+import grid from '../assets/icons/grid.svg';
 
-const HeroImage = (props) => 
-
+const HeroImage = props => (
   <div>
     <Banner
-      color='white'
+      color="white"
       // bg='green4'
-      backgroundImage={rikersCell}
+      backgroundImage="https://i.imgur.com/zY3OaVC.jpg"
     >
       <Flex wrap>
-        
-        <Box
-          w={[1, 1/2]}
-        >
-          <Heading
-              f={9}
-          >
-            {props.loading ?
-              <img src={grid} /> :
-              props.data.length
-            }
+        <Box w={[1, 1 / 2]}>
+          <Heading f={9}>
+            {props.loading ? <img src={grid} /> : props.data.length}
           </Heading>
         </Box>
 
-        <Box
-          w={[1, 1/2]}
-        >
+        <Box w={[1, 1 / 2]}>
           <Text
             mt={3}
             f={[3, 4]}
-            children='persons are in NYC DOC custody today'
+            children="persons are in NYC DOC custody today"
           />
         </Box>
-        
       </Flex>
-
     </Banner>
-
   </div>
+);
 
-export default HeroImage
+export default HeroImage;
