9c506f648daecc2e81c286889f90d6d68b76ea53
Adam Friedl <adamfriedl@Adams-MacBook-Pro.local>
2018-01-08 12:54:38 -0500

    added victory package

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,41 +1,30 @@
-import React from 'react'
-import { PanelHeader, PanelFooter, Text, Box, Subhead } from 'rebass'
-import { VictoryPie } from 'victory'
+import React from 'react';
+import { PanelHeader, PanelFooter, Text, Box, Subhead } from 'rebass';
+import { VictoryPie } from 'victory';
 
-const PieChart = ({
-  title = 'Title',
-  chartData = '#'
-}) => {
+const PieChart = ({ title = 'Title', chartData = '#' }) => {
+  const colors = ['#da864a', '#cc5500', '#a04300', '#5e2700'];
 
-    const colors = [
-    '#da864a',
-    '#cc5500',
-    '#a04300',
-    '#5e2700'
-  ]
-
-  return(
-    <Box
-      w={[1, 1/2, 1/3]}
-      my={2}
-      pr={4}
-    >
-      <PanelHeader
-        color='blue4'
-        f={4}
-        mb={-3}
-        >
+  return (
+    <Box w={[1, 1 / 2, 1 / 3]} my={2} pr={4}>
+      <PanelHeader color="blue4" f={4} mb={-3}>
         {title}
-      </PanelHeader>                        
-        <VictoryPie
-          data={chartData}      
-          innerRadius={25} 
-          labelRadius={50}
-          style={{ labels: { fontFamily: '"Comfortaa", serif', fontSize: 17, fill: "white" }}}
-          colorScale={colors}       
-        />
+      </PanelHeader>
+      <VictoryPie
+        data={chartData}
+        innerRadius={25}
+        labelRadius={50}
+        style={{
+          labels: {
+            fontFamily: '"Comfortaa", serif',
+            fontSize: 17,
+            fill: 'white'
+          }
+        }}
+        colorScale={colors}
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default PieChart
+export default PieChart;
