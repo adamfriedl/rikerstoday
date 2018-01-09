057d9fb0f4f7f0a809d01bfe33d0b7131f606bde
Adam Friedl <adamfriedl@Adams-MacBook-Pro-3.local>
2018-01-09 16:02:20 -0500

    added api key to fetch

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,36 @@
-import React, { Component } from 'react'
-import fetch from 'isomorphic-fetch'
-import { Container } from 'rebass'
-import HeroImage from '../components/HeroImage'
-import Introduction from '../components/Introduction'
-import Header from '../components/Header'
-import Breakdown from '../components/Breakdown'
-import Footer from '../components/Footer'
+import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
+import { Container } from 'rebass';
+import HeroImage from '../components/HeroImage';
+import Introduction from '../components/Introduction';
+import Header from '../components/Header';
+import Breakdown from '../components/Breakdown';
+import Footer from '../components/Footer';
 
 export default class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       loading: true,
       data: []
-    }  
+    };
   }
 
-  componentDidMount() { 
-    fetch('https://data.cityofnewyork.us/resource/gqrb-77i6.json?$limit=12000')
+  componentDidMount() {
+    fetch(
+      'https://data.cityofnewyork.us/resource/gqrb-77i6.json?$limit=11000&$$app_token=DuYlfM56cdZXMxyCoHS5F4bsH'
+    )
       .then(res => res.json())
-      .then(array => 
+      .then(array =>
         this.setState({
           loading: false,
           data: array
         })
-      )
+      );
   }
-  
-  render() {
 
-    const { loading, data } = this.state
+  render() {
+    const { loading, data } = this.state;
 
     return (
       <div>
@@ -41,7 +42,7 @@ export default class App extends Component {
         </Container>
 
         <Footer />
-        </div>
-    )
+      </div>
+    );
   }
 }
